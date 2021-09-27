8960dc47c7a41226241f11268191d8daba695751
John Smith <johnsmithcoder1@gmail.com>
2021-09-27 17:45:12 -0500

    Add placeholder data to StoryPromptTableVC

--- StoryPrompt/View Controllers/StoryPromptTableViewController.swift
+++ StoryPrompt/View Controllers/StoryPromptTableViewController.swift
@@ -9,81 +9,48 @@ import UIKit
 
 class StoryPromptTableViewController: UITableViewController {
 
+    var storyPrompts = [StoryPromptEntry]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let storyPrompt1 = StoryPromptEntry()
+        let storyPrompt2 = StoryPromptEntry()
+        let storyPrompt3 = StoryPromptEntry()
+        
+        storyPrompt1.noun = "toaster"
+        storyPrompt1.adjective = "smelly"
+        storyPrompt1.verb = "attacks"
+        storyPrompt1.number = 5
+        
+        storyPrompt2.noun = "toaster"
+        storyPrompt2.adjective = "smelly"
+        storyPrompt2.verb = "attacks"
+        storyPrompt2.number = 5
+        
+        storyPrompt3.noun = "toaster"
+        storyPrompt3.adjective = "smelly"
+        storyPrompt3.verb = "attacks"
+        storyPrompt3.number = 5
+        
+        storyPrompts = [storyPrompt1, storyPrompt2, storyPrompt3]
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return storyPrompts.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StoryPromptCell", for: indexPath)
+        cell.textLabel?.text = "Prompt \(indexPath.row + 1)"
+        cell.imageView?.image = storyPrompts[indexPath.row].image
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
