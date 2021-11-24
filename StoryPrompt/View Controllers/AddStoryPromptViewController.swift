53b95de7c3045c5ebae10bddd9d6b629f2f64173
John Smith <johnsmithcoder1@gmail.com>
2021-11-23 20:10:50 -0600

    Remove change image functionality

--- StoryPrompt/View Controllers/AddStoryPromptViewController.swift
+++ StoryPrompt/View Controllers/AddStoryPromptViewController.swift
@@ -53,28 +53,12 @@ class AddStoryPromptViewController: UIViewController {
         super.viewDidLoad()
         
         numberSlider.value = 7.5
-        storyPromptImageView.isUserInteractionEnabled = true
-        
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeImage))
-        storyPromptImageView.addGestureRecognizer(gestureRecognizer)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(updateStoryPrompt), name: UIResponder.keyboardDidHideNotification, object: nil)
     }
     
     @objc func updateStoryPrompt() {
         storyPrompt.noun = nounTextField.text ?? ""
         storyPrompt.adjective = adjectiveTextField.text ?? ""
         storyPrompt.verb = verbTextField.text ?? ""
-    }
-    
-    @objc func changeImage() {
-        var configuration = PHPickerConfiguration()
-        configuration.filter = .images
-        configuration.selectionLimit = 1
-        
-        let controller = PHPickerViewController(configuration: configuration)
-        controller.delegate = self
-        present(controller, animated: true)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -91,24 +75,5 @@ extension AddStoryPromptViewController: UITextFieldDelegate {
         textField.resignFirstResponder()
         
         return true
-    }
-}
-
-extension AddStoryPromptViewController: PHPickerViewControllerDelegate {
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        if !results.isEmpty {
-            let result = results.first!
-            let itemProvider = result.itemProvider
-            
-            if itemProvider.canLoadObject(ofClass: UIImage.self) {
-                itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
-                    guard let image = image as? UIImage else { return }
-                    
-                    DispatchQueue.main.async {
-                        self?.storyPromptImageView.image = image
-                    }
-                }
-            }
-        }
     }
 }
