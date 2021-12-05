bb0da86ca4c2967f8f8b906e3d28d08c18c7b9c7
John Smith <johnsmithcoder1@gmail.com>
2021-12-05 17:09:49 -0600

    Fix name of a text variable

--- StoryPrompt/Model/StoryPrompts.swift
+++ StoryPrompt/Model/StoryPrompts.swift
@@ -20,7 +20,7 @@ class StoryPrompts {
     static var stubs = [
         [
             "The <adjective> <noun> saved the day when they <verb> over <number> oceans to return the crown to the princess.",
-            "There once was a hero named <noun>, who was very wise. <noun> left home to go on a very <adjective> adventure. After travelling for <number> years, the <noun> finally <action> all the way back home.",
+            "There once was a hero named <noun>, who was very wise. <noun> left home to go on a very <adjective> adventure. After travelling for <number> years, the <noun> finally <verb> all the way back home.",
             "<number> years ago, there was a very <adjective> storm, but <noun> wasn't scared. <noun> knew exactly what to do. They had to <verb> up to the clouds in order to quiet the storm and save the day. The end."
         ],
         [
